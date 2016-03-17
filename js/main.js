9075aa0ff382cba15cf075fb498de524d53785df
AndreaDuh <xAsianRejectx@gmail.com>
2016-03-17 01:58:05 -0700

    Edited portfolio site

--- js/main.js
+++ js/main.js
@@ -15,3 +15,8 @@ $(document).ready(function(){
     }
   });
 });
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
